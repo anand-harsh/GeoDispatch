85373fdcbefd89fa775016d5451802f26c3d23a0
HARSH ANAND <94885893+anand-harsh@users.noreply.github.com>
2025-02-04 05:07:56 +0530

    add : Ride entity

---
+++ src/main/java/com/floxcon/project/caver/fccabApp/entities/Ride.java
@@ -0,0 +1,45 @@
+package com.floxcon.project.caver.fccabApp.entities;
+
+import com.floxcon.project.caver.fccabApp.entities.enums.PaymentMethod;
+import com.floxcon.project.caver.fccabApp.entities.enums.RideRequestStatus;
+import com.floxcon.project.caver.fccabApp.entities.enums.RideStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalDateTime;
+
+public class Ride {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "Geometry(Point, 4326)")
+    private Point pickupLocation;
+
+    @Column(columnDefinition = "Geometry(Point, 4326)")
+    private Point dropOffLocation;
+
+    @CreationTimestamp
+    private LocalDateTime createdTime; // when driver accepts the ride
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Rider rider;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Driver driver;
+
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+
+    @Enumerated(EnumType.STRING)
+    private RideRequestStatus rideRequestStatus;
+
+    @Enumerated(EnumType.STRING)
+    private RideStatus rideStatus;
+
+    private Double fare;
+
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+}
